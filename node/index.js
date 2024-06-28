0226a33cac18b8d8d2bc42193f4e0ae883ca4f20
alef Silveira <alefo28@gmail.com>
2024-06-28 13:23:36 -0600

    Add Desafio com Nodejs

---
+++ node/index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb"
+}
+const mysql = require("mysql")
+
+const connection = mysql.createConnection(config)
+
+/* const sql = `INSERT INTO people(name) VALUES('alef')`;
+connection.query(sql) */
+let peopleHtml
+connection.query(`SELECT * FROM people`, (error, results, fields) => {
+    if (error) {
+        return;
+    }
+
+    /*peopleHtml = results.map(element => `<p>${element.name}</p>`).join('');*/
+
+    peopleHtml = `<p> ${results[1].name}</p>`
+
+});
+connection.end()
+
+app.get("/", async (req, res) => {
+
+    res.send(`<div><h1>Full Cycle Rocks!</h1>${peopleHtml}</div>`);
+})
+
+
+app.listen(port, () => {
+    console.log("PORT", port);
+})
